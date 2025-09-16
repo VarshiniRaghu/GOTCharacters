2e39cb1d4095ee47b2707faafbff780073ee0126
Varshini Raghu <varshini.divakar@gmail.com>
2025-09-16 07:48:25 +0100

    Layout changes with hardcoded text

--- app/src/main/java/com/example/gotcharactersapp.ui/CharactersScreen.kt
+++ app/src/main/java/com/example/gotcharactersapp.ui/CharactersScreen.kt
@@ -1,31 +1,48 @@
 package com.example.gotcharactersapp.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gotcharactersapp.ui.theme.GOTCharactersAppTheme
 
 @Composable
-internal fun CharactersScreen(){
+internal fun CharactersScreen() {
     GOTCharactersAppTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            DisplayText(
-                name = "Stark",
-                modifier = Modifier.padding(innerPadding)
-            )
+        Card(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        ) {
+            Column {
+                DisplayText(
+                    "Name", 30, Modifier
+                        .padding(bottom = 16.dp)
+                        .fillMaxWidth()
+                )
+                DisplayText(
+                    "Show", 15, Modifier
+                        .padding(bottom = 16.dp)
+                        .fillMaxWidth()
+                )
+            }
         }
     }
 }
 
 @Composable
-fun DisplayText(name: String, modifier: Modifier = Modifier) {
+fun DisplayText(name: String, fontSize: Int, modifier: Modifier = Modifier) {
     Text(
-        text = "GOT Characters $name!",
-        modifier = modifier
+        text = name,
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        fontSize = fontSize.sp
     )
 }
 
@@ -33,6 +50,6 @@ fun DisplayText(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun DisplayTextPreview() {
     GOTCharactersAppTheme {
-        DisplayText("Stark")
+        DisplayText("Stark", 30)
     }
 }
